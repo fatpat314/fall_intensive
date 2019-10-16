fc150fa0459ba649af7447e46a1754be0087b288
Patrick <w.patrick.kelly@gmail.com>
2019-10-15 23:06:04 -0700

    comment form is up but not showing comments

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from data import Articles
+
+#client = MongoClient()
+#db = client.Playlister
+#playlists = db.playlists
 
 app = Flask(__name__)
 
+Articiles = Articles()
+
+# @app.route('/')
+# def index():
+#     """Return homepage"""
+#     return render_template('playlists_index.html', playlists=playlists.find)
+
+# playlists = [
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop beleving'}
+# ]
+
 @app.route('/')
 def index():
-    """Return homepage"""
-    return render_template('home.html', msg='Flask is Cool!!')
-
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop beleving'}
-]
-
-@app.route('/playlists')
-def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('home.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Artical One',
+            'body':'fsfssfsf',
+            'author':'Brad',
+            'create_date':'4-25-2015'
+        },
+        {
+            'id':2,
+            'title':'Artical Two',
+            'body':'fsfssfsf',
+            'author':'Chad',
+            'create_date':'4-30-2015'
+        },
+        {
+            'id':3,
+            'title':'Artical Three',
+            'body':'fsfssfsf',
+            'author':'Brad',
+            'create_date':'5-7-2015'
+        }
+    ]
+    return articles
