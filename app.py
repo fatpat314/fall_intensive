2776de7f0ac16d23f72336f66d523b1d5565d63d
Patrick <w.patrick.kelly@gmail.com>
2019-10-15 19:38:39 -0700

    routes

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ def index():
     """Return homepage"""
     return render_template('home.html', msg='Flask is Cool!!')
 
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop beleving'}
+]
+
+@app.route('/playlists')
+def playlists_index():
+    """Show all playlists."""
+    return render_template('playlists_index.html', playlists=playlists)
+
 if __name__ == '__main__':
     app.run(debug=True)
